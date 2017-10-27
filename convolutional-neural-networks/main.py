911725d0c58f2bf37d64d9d6c7d4e11d39e90459
dlmacedo <dlm@cin.ufpe.br>
2017-10-26 23:50:52 -0300

    Some minor typo...

--- convolutional-neural-networks/main.py
+++ convolutional-neural-networks/main.py
@@ -33,12 +33,12 @@ transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)), # <<-- the original repository is wrong!!!
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)), # <<-- the original repository is wrong!!!
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
